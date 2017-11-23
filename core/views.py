151103aa2604a15773c34c1f230c207ab259d298
thay_pam <thaynarah100@gmail.com>
2017-11-23 06:03:27 -0300

    Meu Primeiro git

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.http import  HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    #return HttpResponse("olá mundo!")
+    return render (request,'index.html')
